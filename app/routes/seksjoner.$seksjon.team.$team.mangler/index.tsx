a8b676370133c25b0b001691bd291f21572472fe
eirikmol <eirik.molnes@nav.no>
2026-08-21 17:54:00 +0200

    Rett grammatikk: 'mangler vurdering' for konsistens med linjen over

--- app/routes/seksjoner.$seksjon.team.$team.mangler/index.tsx
+++ app/routes/seksjoner.$seksjon.team.$team.mangler/index.tsx
@@ -71,7 +71,7 @@ export default function TeamComplianceGaps() {
 					Mangler
 				</Heading>
 				<BodyShort textColor="subtle">{gaps.length} kontroller mangler vurdering</BodyShort>
-				<BodyShort textColor="subtle">{economyGapCount} kontroller mangler vurderinger på økonomisystemer</BodyShort>
+				<BodyShort textColor="subtle">{economyGapCount} kontroller mangler vurdering på økonomisystemer</BodyShort>
 			</VStack>
 
 			<HStack gap="space-4" wrap>
